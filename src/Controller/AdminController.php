711fb6d92cdee692e3fbe2d56f7b571932c86015
Branimir Butković <56635583+Branimir97@users.noreply.github.com>
2022-01-26 17:23:05 +0100

    Created data table

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 /**
  * @Route("/admin")
@@ -16,14 +18,20 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
         return $this->render('admin/index.html.twig');
     }
 
     /**
-     * @Route("/delete", name="delete_account")
+     * @Route("/delete", name="account_delete")
      */
-    public function delete() 
+    public function delete(Request $request, User $user) 
     {
-
+        $this->denyAccessUnlessGranted("ROLE_ADMIN");
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) 
+        {
+            $this->doctrine->remove($user);
+            $this->doctrine->flush();
+        }
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             );
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('auth/register.html.twig', [
