ef89efe60a86f324495993a7e34e466f8717c1e4
Branimir Butković <56635583+Branimir97@users.noreply.github.com>
2022-01-26 12:13:35 +0100

    Created Admin entity and migrated to db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $ip = $request->getClientIp();
+        $browserLanguage = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        dd($browserLanguage);
         return $this->render('home/index.html.twig');
     }
 }
