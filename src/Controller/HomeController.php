4bc01fd16d4b72d5d2e71999fac7d4a2cdb1139d
Branimir Butković <56635583+Branimir97@users.noreply.github.com>
2022-01-27 15:31:44 +0100

    Created Visitor Entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,9 +14,11 @@ class HomeController extends AbstractController
      */
     public function index(Request $request): Response
     {
-        // $ip = $request->getClientIp();
-        // $browserLanguage = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        // dd($browserLanguage);
+        $ip = "46.188.144.192";
+
+        $ipdata = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+        dd($ipdata);
         return $this->render('home/index.html.twig');
     }
 }
